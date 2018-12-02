aa952000c72473c50a0eecca19f56fbf2fb4c26a
Vladislav Gapurov <gapurov@gmail.com>
2018-12-02 02:47:24 +0100

    Add aliases-tmp source

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -42,6 +42,7 @@ test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell
 source $HOME/.dotfiles/zsh/fns.zsh
 source $HOME/.dotfiles/zsh/paths.zsh
 source $HOME/.dotfiles/zsh/aliases.zsh
+source $HOME/.dotfiles/zsh/aliases-tmp.zsh
 
 ##############################################################################
 # History Configuration
