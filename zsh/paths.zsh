422dec62e2af07f10b4dbdf2fd07ea3db71682fe
Vladislav Gapurov <gapurov@gmail.com>
2020-03-03 15:54:46 +0100

    Update paths

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,6 +2,7 @@
 export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
