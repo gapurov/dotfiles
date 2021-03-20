dab5441d8b4ef983e0d7bb589d6428f7b2a39dbf
Vladislav Gapurov <gapurov@gmail.com>
2021-03-20 18:21:30 +0100

    Add powerlevel10k theme to the config

--- install.sh
+++ install.sh
@@ -33,6 +33,22 @@ echo -e "\033[1m\033[34m==> Install global JS packages\033[0m"
 echo -e "\033[1m\033[34m==> Install oh-my-zsh\033[0m"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Wait a bit before moving on...
+sleep 2
+
+# Install powerlevel10k theme
+echo -e "\033[1m\033[34m==> Install powerlevel10k theme\033[0m"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Wait a bit before moving on...
+sleep 2
+
+echo -e "\033[1m\033[34m==> Link .p10k.zsh\033[0m"
+ln -sf ~/.dotfiles/zsh/p10k.zsh ~/.p10k.zsh
+
+# Wait a bit before moving on...
+sleep 2
+
 echo -e "\033[1m\033[34m==> Link .zshrc\033[0m"
 ln -sf ~/.dotfiles/zsh/zshrc.zsh ~/.zshrc
 
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
@@ -10,7 +17,8 @@ export ZSH=$HOME/.oh-my-zsh
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+# https://github.com/romkatv/powerlevel10k
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 plugins=(
   jsontools
@@ -43,3 +51,6 @@ HISTDUP=erase               #Erase duplicates in the history file
 setopt    appendhistory     #Append history to the history file (no overwriting)
 setopt    sharehistory      #Share history across terminals
 setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
