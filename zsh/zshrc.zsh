bee9c19744ca94a6302f66c79c7677337f747400
Vladislav Gapurov <gapurov@gmail.com>
2025-09-06 17:07:49 +0200

    Update

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -73,3 +73,9 @@ if [[ "$TERM_PROGRAM" == "vscode" ]]; then
   PROMPT='%n@%m:%~%# '
   RPROMPT=''
 fi
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/path.zsh.inc' ]; then . '/opt/homebrew/share/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/completion.zsh.inc' ]; then . '/opt/homebrew/share/google-cloud-sdk/completion.zsh.inc'; fi
